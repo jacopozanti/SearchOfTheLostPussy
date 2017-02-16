380021427770494fa0ce8bb73138048a85c9fd1e
ZANTI JACOPO <j.zanti@VINCI.IISCORNI.IT>
2017-02-16 08:57:44 +0100

    continuo gioco

--- SearchOfTheLostPussy/src/Controller/CollisionDetectorWall.java
+++ SearchOfTheLostPussy/src/Controller/CollisionDetectorWall.java
@@ -5,6 +5,7 @@
  */
 package Controller;
 
+import Component.GameObject;
 import Model.ElementInGame;
 import View.Wall;
 
@@ -21,8 +22,12 @@ public class CollisionDetectorWall extends Thread {
             for (Object object : ElementInGame.WallInGame) 
             {
                 Wall wall = ((Wall)object);
-                for (Object object : col) {
-                    
+                for (Object objectIn : ElementInGame.ElementInGame) {
+                    GameObject gameObject = ((GameObject)objectIn);
+                    if(gameObject.hitBox.intersects(wall.hitBox))
+                    {
+                        //riscrivere intersecs nella hitbox class in modo da trovare la direzione
+                    }
                 }
             }
         }
